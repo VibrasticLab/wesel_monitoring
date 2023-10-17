f89851da2944ef4484cbb0bc5374b2d801150f1e
maasyraf <maasyraf.edu@gmail.com>
2023-10-17 15:52:53 +0700

    add spectrum plot

--- interface/python/plot_test/serialplot.py
+++ interface/python/plot_test/serialplot.py
@@ -68,13 +68,35 @@ class SerialPlotTest():
         self.X = np.arange(0, self.DataLong, 1)
         self.Y = np.zeros(self.DataLong, dtype='i2')
 
+        # Graph FFT Data
+        self.fs = 4000
+        self.nfft = int(pow(2, np.ceil(np.log2(len(self.Y)))))
+        self.win = np.hamming(self.nfft)
+        self.freq = (self.fs / 2) * np.arange(0, 1, 1/(self.nfft/2+1))
+        self.amp = np.zeros(int(self.nfft/2+1), dtype='i2')
+
         # Example Figure Plot
-        self.fig = Figure(figsize=(11, 9), dpi=100,facecolor='white')
-        self.ax = self.fig.add_subplot(111)
-        self.ax.set_facecolor('white')
-        self.ax.grid(True,which='both',ls='-')
-        self.ax.set_ylim(1500,2500)
-        self.line, = self.ax.plot(self.X, self.Y)
+        self.fig = Figure(figsize=(11, 9), dpi=100,facecolor='white', tight_layout=True)
+
+        # Time domain Plot
+        self.ax1 = self.fig.add_subplot(211)
+        self.ax1.set_facecolor('white')
+        self.ax1.grid(True,which='both',ls='-')
+        self.ax1.set_ylim(1500,2500)
+        self.ax1.set_title("Vibration in Time domain")
+        self.ax1.set_xlabel("Data point")
+        self.ax1.set_ylabel("Amplitude")
+        self.line1, = self.ax1.plot(self.X, self.Y)
+
+        # Spectrum domain Plot
+        self.ax2 = self.fig.add_subplot(212)
+        self.ax2.set_facecolor('white')
+        self.ax2.grid(True,which='both',ls='-')
+        self.ax2.set_ylim(0,10000)
+        self.ax2.set_title("Vibration in Spectrum domain")
+        self.ax2.set_xlabel("FFT point")
+        self.ax2.set_ylabel("Magnitude")
+        self.line2, = self.ax2.plot(self.freq, self.amp)
 
         style.use('ggplot')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.graphfrm)
@@ -117,6 +139,7 @@ class SerialPlotTest():
         self.DataIdx = self.DataIdx + 1
         if self.DataIdx == self.DataLong:
             self.DataIdx = 0
+            self.amp = np.abs(np.fft.fft(self.win * self.Y))[0:int(self.nfft/2+1)]
 
     def wnd_closing(self):
         self.SerThdRun = False
@@ -136,7 +159,8 @@ class SerialPlotTest():
 
     def graphupdate(self,args):
         if self.PlotUpd:
-            self.line.set_data(self.X,self.Y)
+            self.line1.set_data(self.X,self.Y)
+            self.line2.set_data(self.freq,self.amp)
 
 if __name__ == "__main__":
     serplot = SerialPlotTest()
